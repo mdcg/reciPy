4ba3ce9be030788a560723654f268ada460f1eed
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-08-29 00:03:51 -0300

    Creating models for recipes

--- src/api/models.py
+++ src/api/models.py
@@ -16,6 +16,7 @@ class Recipe(BaseModel):
     name = models.CharField(max_length=255)
     description = models.TextField()
     steps = models.TextField()
+    image = models.ImageField(upload_to='recipes/')
     difficulty = models.IntegerField(
         default=1,
         validators=[MaxValueValidator(10), MinValueValidator(1)]

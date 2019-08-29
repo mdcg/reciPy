fe6e4899da33576055e301b1ca9ec298a5fb8b65
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-08-29 00:05:20 -0300

    Serving images URL

--- src/core/urls.py
+++ src/core/urls.py
@@ -21,4 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v0/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

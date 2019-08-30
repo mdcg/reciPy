5e37066cbe272bda08d44d1c6bc9fc9507123f39
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-08-30 01:28:56 -0300

    Creating recipes urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from api.views.users.authentication_views import SignInView, SignUpView
+from api.views.recipes.recipes_manage_views import UserRecipesView, RecipesView
+from api.views.users.users_authentication_views import SignInView, SignUpView
 
 urlpatterns = [
     path('signup', SignUpView.as_view(), name='signup'),
     path('signin', SignInView.as_view(), name='signin'),
+    path('recipes', RecipesView.as_view(), name='recipes'),
+    path('me/recipes', UserRecipesView.as_view(), name='user-recipes'),
 ]

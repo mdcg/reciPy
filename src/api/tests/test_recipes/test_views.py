0e9d8b6cfd0eac25a4160440efe41ddf65810dcc
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-08-31 00:02:32 -0300

    Refactoring recipes tests

--- src/api/tests/test_recipes/test_views.py
+++ src/api/tests/test_recipes/test_views.py
@@ -1,17 +1,14 @@
-import tempfile
+import random
 
 from django.urls import reverse
-from faker import Factory
-from PIL import Image
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
-from core import settings
 
 from api.seeds.recipes_seeds import RecipeFactory
 from api.seeds.users_seeds import UserFactory
-
-faker = Factory.create()
+from api.serializers.recipes_serializers import RecipeSerializer
+from core import settings
 
 
 class RecipesViewsAPITestCase(APITestCase):
@@ -23,31 +20,24 @@ class RecipesViewsAPITestCase(APITestCase):
     def test_create_new_recipe(self):
         url = reverse('user-recipes')
 
-        new_image = self.generate_image()
-
-        with open(new_image.name, 'rb') as img:
-            data = {
-                'name': faker.sentence(
-                    nb_words=4,
-                    variable_nb_words=True,
-                    ext_word_list=None
-                ),
-                'description': faker.text(),
-                'steps': faker.text(),
-                'difficulty': faker.random_int(1, 10),
-                'image': img,
-            }
+        recipe_image = RecipeFactory.generate_recipe_image()
+        with open(recipe_image.name, 'rb') as img:
+            data = RecipeFactory.generate_recipe_json_data(recipe_image=img)
 
             response = self.client.post(url, data, format='multipart')
 
+            self.assertEqual(response.data.get('status'), 'success')
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_list_all_user_recipes(self):
-        self.recipe = RecipeFactory(user=self.user)
+        url = reverse('user-recipes')
 
-    def generate_image(self):
-        # Create image
-        image = Image.new('RGB', (100, 100))
-        tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
-        image.save(tmp_file)
-        return tmp_file
+        number_of_recipes = random.randint(1, 10)
+
+        RecipeFactory.create_batch(number_of_recipes, user=self.user)
+
+        response = self.client.get(url, {}, format='json')
+
+        self.assertEqual(response.data.get('status'), 'success')
+        self.assertEqual(response.data.get('payload').get('count'), number_of_recipes)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
